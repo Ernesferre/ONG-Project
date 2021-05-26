c89bbbe853646fcf337cb61cd68ee593b37bbf74
ruggiadufour <angelruggia@gmail.com>
2021-05-26 19:42:59 -0300

    Solving merge incidences

--- src/Components/backoffice/layout/Loader.js
+++ src/Components/backoffice/layout/Loader.js
@@ -3,7 +3,7 @@ import LoaderSpinner from "react-loader-spinner";
 
 const Loader = (props) => {
   //Documentation: https://www.npmjs.com/package/react-loader-spinner
-  //Example: <Loader visible={true} type="Bars" color="red" height={80} width={80} />
+  //Example: <Loader visible={true} type="Bars" color="red" height={80} width={80}/>
   return <LoaderSpinner {...props} />;
 };
 
