8874100b05e6e0f840207f9f23791cb660c6420f
GastonLell <gastonglell@gmail.com>
2021-05-26 19:34:32 -0300

    layout of login form and incorporation of formik

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import {
+  Flex,
+  Heading,
+  Stack,
+  Input,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+} from "@chakra-ui/react";
 
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    )
-}
+  const validate = Yup.object({
+    email: Yup.string().email("El email es invalido").required("Requerido"),
+    password: Yup.string().required("Requerido"),
+  });
+  return (
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={validate}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {(formik) => (
+        <Flex
+          direction="column"
+          height="90vh"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Stack bg="gray.100" p={12} rounded={6}>
+            <Stack mb={6}>
+              <Heading>Formulario de registro</Heading>
+            </Stack>
 
-export default Login
+            <Form spacing={6} w="100%">
+              <FormControl  id="email" w="100%">
+
+                <FormLabel>Email</FormLabel>
+                <Input bg="#fff" type="email" />
+
+              </FormControl>
+
+              <FormControl id="email">
+
+                <FormLabel>Contraseña</FormLabel>
+                <Input  bg="#fff" type="email" />
+                
+              </FormControl>
+            </Form>
+          </Stack>
+        </Flex>
+      )}
+    </Formik>
+  );
+};
+
+export default Login;
