f0f460ee80a7344c611ec4dff8e0c9ec65a7519a
jorge miranda <jorgemiranda1977@gmail.com>
2021-05-28 11:58:06 -0300

    iniciando store y authReducer

---
+++ src/features/authReducer.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authReducer = createSlice({
+  name: "register",
+  initialState: {
+ 
+ 
+  },
+  reducers: {
+   
+  },
+});
+
+
+
+export default authReducer.reducer;
