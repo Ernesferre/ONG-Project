6ea274cf2693656b79d7cd731775d0aa702ece5a
AlanProgrammer93 <alanwebprogrammer@gmail.com>
2021-05-31 20:50:14 -0300

    separe las rutas publicas de las privadas creando 2 componentes mas en una carpeta routes

---
+++ src/routes/Private.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom'
+
+import CreateActivity from '../components/backoffice/activities/CreateActivity'
+import EditActivity from '../components/backoffice/activities/EditActivity'
+import ListOfActivities from '../components/backoffice/activities/ListOfActivities'
+import Backoffice from '../components/backoffice/Backoffice'
+import { CategoryList } from '../components/backoffice/categories/CategoryList'
+import Header from '../components/backoffice/layout/Header'
+import NoveltiesList from '../components/backoffice/novelties/NoveltiesList'
+import SlidesShow from '../components/backoffice/slides/SlidesShow'
+import { CategoryCreate } from '../pages/backoffice/categories/CategoryCreate'
+import { CategoryPatch } from '../pages/backoffice/categories/CategoryPatch'
+
+export const Private = () => {
+    return (
+        <>
+          <Header />
+          <Switch>
+            <Route exact path="/backoffice" component={Backoffice} />
+            <Route exact path="/backoffice/news" component={NoveltiesList}/>
+            <Route
+              exact
+              path="/backoffice/categories"
+              component={CategoryList}
+            />
+            <Route
+              exact
+              path="/backoffice/categories/create"
+              component={CategoryCreate}
+            />
+            <Route
+              exact
+              path="/backoffice/categories/:id"
+              component={CategoryPatch}
+            />
+            
+            <Route
+              exact
+              path="/backoffice/activities"
+              component={ListOfActivities}
+            />
+            <Route
+              exact
+              path="/backoffice/activities/create"
+              component={CreateActivity}
+            />
+            <Route
+              exact
+              path="/backoffice/activities/edit"
+              component={EditActivity}
+            />
+            <Route
+              exact
+              path="/backoffice/slides"
+              component={SlidesShow}
+            />
+
+            <Redirect to="/" />
+
+          </Switch>
+        </>
+        
+    )
+}
