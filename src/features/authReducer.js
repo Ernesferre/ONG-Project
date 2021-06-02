21cac37b2cf6dca41fc2c910f4a1fdfd6074fff4
Ernesferre <70835371+Ernesferre@users.noreply.github.com>
2021-06-02 13:34:55 -0300

    Eliminate user data and set as false authentication with SET_LOGOUT function located in authReducer

--- src/features/authReducer.js
+++ src/features/authReducer.js
@@ -16,6 +16,13 @@ export const authReducer = createSlice({
     },
     SET_REGISTER: (state, action) => {
       state.data = action.payload
+   },
+
+   SET_LOGOUT: (state) => {
+     state.data = null
+        // CUANDO SE DEFINA LA VARIABLE "LOGGED" EN EL INITIAL STATE ACTIVO LA LINEA DE ABAJO
+        
+    //  state.logged = false
    }
 
   },
@@ -23,7 +30,7 @@ export const authReducer = createSlice({
 
 export const {SET_LOGIN} = authReducer.actions;
 export const {SET_REGISTER} = authReducer.actions;
-
+export const {SET_LOGOUT} = authReducer.actions;
 
 export const selectToken = state => state.register.data;
 
