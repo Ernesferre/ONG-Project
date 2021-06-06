4efc7a03eeb1565bc5ef930dec9d325b3eab2699
lmv <vazquez.leo.m@gmail.com>
2021-06-06 15:11:27 -0300

    UserList uses redux to fetch users

--- src/components/backoffice/users/MappedUsers.js
+++ src/components/backoffice/users/MappedUsers.js
@@ -5,10 +5,8 @@ import { Link } from "react-router-dom";
 import React from "react";
 import Swal from "sweetalert2";
 
-
 export const MappedUsers = ({ users, handleUpdate }) => {
   const flexDir = useBreakpointValue({ base: "column", sm: "" });
-
   const handleDelete = async (id) => {
     const confirmation = await Swal.fire({
       title: "Confirmación",
@@ -21,17 +19,17 @@ export const MappedUsers = ({ users, handleUpdate }) => {
     });
     if (confirmation.isConfirmed === true) {
       // DELETE FUNCTION HERE
-        console.log("delete")
-        handleUpdate()
+      console.log("delete");
+      handleUpdate();
     }
   };
 
   return (
     <Flex flexDir="column">
-      {users.map((user) => (
+      {users.map((user, index) => (
         <Flex
+          key={index}
           flexDir="column"
-          key={user.id}
           margin="1em"
           bg="gray.200"
           padding="1em"
@@ -42,23 +40,31 @@ export const MappedUsers = ({ users, handleUpdate }) => {
               <Text fontSize="small" color="gray.400">
                 Usuario:
               </Text>
-              <Text fontWeight="bold">{`\u00A0\u00A0${user.name.charAt(0).toUpperCase() + user.name.slice(1)}`}</Text>
+              <Text
+                fontWeight="bold"
+                textTransform="capitalize"
+                marginLeft="0.3em"
+              >
+                {user.name}
+              </Text>
             </Flex>
             <Flex alignItems="center" justifyContent="space-between">
               <Text fontSize="small" color="gray.400">
                 Email:
               </Text>
-              <Text fontWeight="bold">{`\u00A0\u00A0${user.email.charAt(0).toUpperCase() + user.email.slice(1)}`}</Text>
+              <Text fontWeight="bold" marginLeft="0.3em">
+                {user.email}
+              </Text>
             </Flex>
             <Flex justifyContent="space-between">
-              <Link 
-                  to={{
-                    pathname: "/backoffice/users/edit",
-                    state: {user: user},
-                 
-                }}>
+              <Link
+                to={{
+                  pathname: "/backoffice/users/edit",
+                  state: { user: user },
+                }}
+              >
                 <Button colorScheme="blue" size="sm" variant="outline">
-                Editar
+                  Editar
                 </Button>
               </Link>
               <Button
--- src/components/backoffice/users/UserList.js
+++ src/components/backoffice/users/UserList.js
@@ -1,58 +1,39 @@
 import { Button } from "@chakra-ui/button";
 import { Container, Flex, Heading } from "@chakra-ui/layout";
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import {  MappedUsers} from "./MappedUsers";
-
-
-const fakeData = [
-  { id:1, name: "user 1", email: "user1@user.com",  role: "admin", profilePhoto:"https://images.unsplash.com/photo-1495446815901-a7297e633e8d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80", created_at: "2021-05-12T00:59:46.326Z" },
-  { id:2, name: "user 2", email: "user2@user.com",  role: "user", profilePhoto:"https://images.unsplash.com/photo-1509191436522-d296cf87d244?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=709&q=80", created_at: "2021-02-28T00:59:46.326Z" },
-  { id:3, name: "user 3", email: "user3@user.com",  role: "admin", profilePhoto:"https://images.unsplash.com/photo-1513364776144-60967b0f800f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80", created_at: "2020-11-05T00:59:46.326Z" },
-  { id:4, name: "user 4", email: "user4@user.com",  role: "admin", profilePhoto:"https://images.unsplash.com/photo-1495446815901-a7297e633e8d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80", created_at: "2021-01-10T00:59:46.326Z" },
-  { id:5, name: "user 5", email: "user5@user.com",  role: "admin", profilePhoto:"https://images.unsplash.com/photo-1509191436522-d296cf87d244?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=709&q=80", created_at: "2020-10-06T00:59:46.326Z" },
-  { id:6, name: "user 6", email: "user6@user.com",  role: "user", profilePhoto:'', created_at: "2021-05-28T00:59:46.326Z" }
-]
+import { fetchUsers } from "../../../features/userSlice";
+import { MappedUsers } from "./MappedUsers";
 
 export const UserList = () => {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.users.userList);
 
-   
+  const [loading, setLoading] = useState(true);
+  const [update, setUpdate] = useState(false);
 
-  const [loading, setLoading] = useState(true)
-  const [users, setUsers] = useState()
-  const [update, setUpdate] = useState(false)
-  const [lastID, setLastID] = useState()
-  
-
-  useEffect(()=>{
-
-          // AGREGAR FUNCIÓN para traer data
-   
-      console.log(fakeData)
-      setUsers(fakeData)
-      setLastID(fakeData.sort((a,b)=> b.id - a.id)[0].id)
-      setLoading(false)
- 
-  }, [update])
+  useEffect(() => {
+    // AGREGAR FUNCIÓN para traer data
+    dispatch(fetchUsers());
+    setLoading(false);
+  }, []);
 
   const handleUpdate = () => {
-    setUpdate(update => !update)
-  }
+    setUpdate((update) => !update);
+  };
 
-  if (loading) return <Heading textAlign="center">Loagind...</Heading>
+  if (loading) return <Heading textAlign="center">Loagind...</Heading>;
   return (
     <Container maxW="container.xl">
       <Flex flexDir="column">
         <Heading textAlign="center">Usuarios</Heading>
         <Flex>
-          <Link to={{
-              pathname: "/backoffice/users/create",
-              state: {lastID:lastID},
-          }}>
+          <Link to="/backoffice/users/create">
             <Button colorScheme="green">+ Nuevo Usuario</Button>
           </Link>
         </Flex>
-        <MappedUsers users={users} handleUpdate={handleUpdate} />
+        <MappedUsers users={users[0]} handleUpdate={handleUpdate} />
       </Flex>
     </Container>
   );
