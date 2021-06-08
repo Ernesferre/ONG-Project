dbeab67c7f8b34653bbb44c13f3f7c68c0538e12
lmv <vazquez.leo.m@gmail.com>
2021-06-08 11:39:11 -0300

    Removed hook from createoredituser

--- src/components/backoffice/users/CreateOrEditUser.js
+++ src/components/backoffice/users/CreateOrEditUser.js
@@ -1,18 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import { CreateOrEditForm } from "./CreateOrEditForm";
 
 export const CreateOrEditUser = ({ isCreate, id, user }) => {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    if (!isCreate) {
-      setLoading(false);
-    } else {
-      setLoading(false);
-    }
-  }, [isCreate, user]);
-
   return (
     <>
       <CreateOrEditForm id={id} />
