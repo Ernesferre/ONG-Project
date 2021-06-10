4027c9bb6222b1b164f199c06ef814d2fd63338b
AlanProgrammer93 <alanwebprogrammer@gmail.com>
2021-06-10 00:46:19 -0300

    Footer de la web publica creada

--- src/components/public/Home/index.js
+++ src/components/public/Home/index.js
@@ -1,30 +1,35 @@
 import React from "react";
 import { Heading, Text, Box } from "@chakra-ui/react";
+import Footer from "../layout/Footer";
 
 const Home = () => {
   return (
-    <Box m={{ base: "1rem", md: "2rem" }}>
-      <Box>
-        <Heading as="h1" size="2xl">
-          Somos Más
-        </Heading>
-        <Text fontSize={{ base: "1rem", md: "1.5rem" }}>
-          Mediante nuestros programas educativos, buscamos incrementar la
-          capacidad intelectual, moral y afectiva de las personas de acuerdo con
-          la cultura y las normas de convivencia de la sociedad a la que
-          pertenecen.
-        </Text>
+    <>
+      <Box m={{ base: "1rem", md: "2rem" }}>
+        <Box>
+          <Heading as="h1" size="2xl">
+            Somos Más
+          </Heading>
+          <Text fontSize={{ base: "1rem", md: "1.5rem" }}>
+            Mediante nuestros programas educativos, buscamos incrementar la
+            capacidad intelectual, moral y afectiva de las personas de acuerdo con
+            la cultura y las normas de convivencia de la sociedad a la que
+            pertenecen.
+          </Text>
+        </Box>
+        <Box h="400px">
+            Slides
+        </Box>
+        <Box h="400px">
+            Novedades
+        </Box>
+        <Box h="400px">
+            Testimonios
+        </Box>
+        
       </Box>
-      <Box h="400px">
-          Slides
-      </Box>
-      <Box h="400px">
-          Novedades
-      </Box>
-      <Box h="400px">
-          Testimonios
-      </Box>
-    </Box>
+      <Footer />
+    </>
   );
 };
 
