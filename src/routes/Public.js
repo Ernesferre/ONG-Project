44218aef79ba51a739dd7f3dcbe2fe12827ccbf6
MiliGaleano <mmiligaleano@gmail.com>
2021-06-11 15:20:02 -0300

    add header and footer to layout component

--- src/routes/Public.js
+++ src/routes/Public.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import HeaderPublic from "../components/public/layout/HeaderPublic";
 import Home from "../components/public/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Us from "../pages/Us";
 import { Contact } from "../components/public/contact";
+import Layout from "../components/public/layout/Layout";
 
 export const Public = () => {
   return (
     <>
-      <HeaderPublic />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/us" component={Us} />
-        <Route exact path="/contacto" component={Contact} />
-        <Redirect to="/" />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/us" component={Us} />
+          <Route exact path="/contacto" component={Contact} />
+          <Redirect to="/" />
+        </Switch>
+      </Layout>
     </>
   );
 };
