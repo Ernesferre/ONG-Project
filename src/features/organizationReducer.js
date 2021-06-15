dc83c3934c96be5d8e05741309dfefca6f4d0bec
MiliGaleano <mmiligaleano@gmail.com>
2021-06-15 11:31:29 -0300

    create organizationReducer to store info in redux

---
+++ src/features/organizationReducer.js
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+const handleError = () => {
+  Swal.fire({
+    title: "Error",
+    text: "Algo salió mal, por favor refresque la página",
+    icon: "error",
+    confirmButtonText: "OK",
+  });
+};
+
+export const fetchOrganizationData = createAsyncThunk("state/fetchOrganizationData", async () => {
+    try {
+        const response = await axios.get(`http://ongapi.alkemy.org/api/organization`);
+        return response.data.data[0];
+    } catch (error) {
+        handleError();
+    }
+  });
+
+export const organizationReducer = createSlice({
+  name: "organization",
+  initialState: {
+    error: "",
+    status: "idle",
+    organizationData: {},
+  },
+    // reducers: {
+    //     deleteOrganizationData: (state) => {
+    //         state.organizationData = {}
+    //     }
+    // },
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchOrganizationData.pending, (state) => {
+            state.status = "loading";
+        })
+        .addCase(fetchOrganizationData.fulfilled, (state, action) => {
+            state.organizationData = action.payload;
+            state.status = "idle";
+        })
+        .addCase(fetchOrganizationData.rejected, (state, action) => {
+            state.error = action.error;
+            state.status = "idle";
+        })
+        }
+});
+
+// export const { deleteOrganizationData } = organizationReducer.actions;
+export default organizationReducer.reducer;
