60b52d96714ce2d6827e804c7f29caf37d5370f0
lmv <vazquez.leo.m@gmail.com>
2021-06-23 11:17:02 -0300

    Added link button to see more activities on PublicActivities

--- src/components/public/activities/PublicActivities.js
+++ src/components/public/activities/PublicActivities.js
@@ -2,12 +2,22 @@ import React from "react";
 import Title from "../../Title/Title";
 import pictureActivities from "../../../assets/Foto10.jpg";
 import { ActivitiesList } from "./ActivitiesList";
+import { Button, Flex } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { FaChevronRight } from "react-icons/fa";
 
 export const PublicActivities = () => {
   return (
     <>
       <Title title="Actividades" image={pictureActivities} />
-      <ActivitiesList />
+      <ActivitiesList getOnlyLatest={true} />
+      <Flex justify="flex-end" margin="1em">
+        <Link to="/actividades">
+          <Button variant="somosMas" rightIcon={<FaChevronRight />}>
+            Ver más actividades
+          </Button>
+        </Link>
+      </Flex>
     </>
   );
 };
