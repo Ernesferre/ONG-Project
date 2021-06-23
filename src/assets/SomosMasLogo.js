989cc17fc78c5624e20feeb97ed4bb88544953dd
lmv <vazquez.leo.m@gmail.com>
2021-06-23 12:07:59 -0300

    Added link to / on SomosMasLogo component

--- src/assets/SomosMasLogo.js
+++ src/assets/SomosMasLogo.js
@@ -1,18 +1,21 @@
 import { Flex, Heading, Image } from "@chakra-ui/react";
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "./brand-logo.svg";
 
 export const SomosMasLogo = ({ maxW, fontSize }) => {
   return (
-    <Flex flexDir="column" maxW={maxW}>
-      <Image src={logo} />
-      <Heading
-        fontSize={fontSize}
-        textTransform="uppercase"
-        fontWeight="semiBold"
-      >
-        Somos Más
-      </Heading>
-    </Flex>
+    <Link to="/">
+      <Flex flexDir="column" maxW={maxW}>
+        <Image src={logo} />
+        <Heading
+          fontSize={fontSize}
+          textTransform="uppercase"
+          fontWeight="semiBold"
+        >
+          Somos Más
+        </Heading>
+      </Flex>
+    </Link>
   );
 };
