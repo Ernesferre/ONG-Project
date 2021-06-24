0cae67666d370823411c6490e4e39030c9145fd6
Ernesferre <70835371+Ernesferre@users.noreply.github.com>
2021-06-24 12:47:50 -0300

    Create Title and Text content in Thanks component

--- src/components/public/donations/Thanks.js
+++ src/components/public/donations/Thanks.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Box, Text, Button } from "@chakra-ui/react";
+import forBackTitle from "../../../assets/forBackTitle.jpg";
+import Title from "../../Title/Title";
 
 const Thanks = () => {
+    
     return (
-        <div>
-            Gracias Totales
-        </div>
+        <Box mb="2rem">
+                <Title image={forBackTitle} title="Gracias" />
+                <Box maxW="900px" m={{base:"1rem",md:"auto"}} my="2rem">
+                <Text
+                mt="3rem"
+                fontSize={{md:"5xl",base:"3xl"}}
+                paddingBottom={6}
+                fontWeight="bold"
+                >
+                ¡Muchas gracias por querer apoyar a la causa! 
+                </Text>
+                
+                </Box>
+        </Box>
     )
 }
 
