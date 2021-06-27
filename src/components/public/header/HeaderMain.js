c00d401e881bd2c78f647d4c4d796da67b3c4658
lmv <vazquez.leo.m@gmail.com>
2021-06-27 18:23:53 -0300

    Adding backoffice

--- src/components/public/header/HeaderMain.js
+++ src/components/public/header/HeaderMain.js
@@ -7,6 +7,7 @@ import { HeaderRoutes } from "./HeaderRoutes";
 import { HeaderMenuBtn } from "./HeaderMenuBtn";
 import { HeaderDrawer } from "./HeaderDrawer";
 import { DonateBtn } from "./DonateBtn";
+import { BackofficeRoutes } from "./backoffice/BackofficeRoutes";
 
 export const HeaderMain = () => {
   const routes = [
@@ -16,6 +17,7 @@ export const HeaderMain = () => {
     { url: "/testimonios", name: "Testimonios" },
   ];
 
+  let isBackoffice = true;
   let username = "test";
   const { isOpen, onOpen, onClose } = useDisclosure();
 
@@ -24,24 +26,34 @@ export const HeaderMain = () => {
       <Flex alignItems="center" margin="1em">
         <SomosMasLogo />
       </Flex>
-      <Flex
-        marginLeft="auto"
-        marginRight="1em"
-        alignItems="center"
-        display={{ base: "none", md: "none", lg: "flex" }}
-      >
-        <ActivitiesMenu />
-        <HeaderRoutes routes={routes} />
-        <AuthLinks username={username} />
-        <DonateBtn />
-      </Flex>
-      <HeaderMenuBtn onOpen={onOpen} />
-      <HeaderDrawer
-        onClose={onClose}
-        isOpen={isOpen}
-        routes={routes}
-        username={username}
-      />
+      {isBackoffice ? (
+        <>
+          <Flex marginLeft="auto" marginRight="1em">
+            <BackofficeRoutes />
+          </Flex>
+        </>
+      ) : (
+        <>
+          <Flex
+            marginLeft="auto"
+            marginRight="1em"
+            alignItems="center"
+            display={{ base: "none", md: "none", lg: "flex" }}
+          >
+            <ActivitiesMenu />
+            <HeaderRoutes routes={routes} />
+            <AuthLinks username={username} />
+            <DonateBtn />
+          </Flex>
+          <HeaderMenuBtn onOpen={onOpen} />
+          <HeaderDrawer
+            onClose={onClose}
+            isOpen={isOpen}
+            routes={routes}
+            username={username}
+          />
+        </>
+      )}
     </Flex>
   );
 };
