207cec9192ebcf47d63e0ef01f73ce1fc82c707b
lmv <vazquez.leo.m@gmail.com>
2021-06-28 17:54:16 -0300

    Changed icon to publicbtn

--- src/components/public/header/PublicBtn.js
+++ src/components/public/header/PublicBtn.js
@@ -1,13 +1,13 @@
 import { Button, Flex } from "@chakra-ui/react";
 import React from "react";
-import { MdComputer } from "react-icons/md";
+import { MdPublic } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 export const PublicBtn = () => {
   return (
     <Flex margin="1em">
       <Link to="/">
-        <Button variant="somosMasOutline" rightIcon={<MdComputer />}>
+        <Button variant="somosMasOutline" rightIcon={<MdPublic />}>
           Ir a Sitio Público
         </Button>
       </Link>
